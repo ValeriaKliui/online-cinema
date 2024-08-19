110f4ca53f95c5d316c880633076259b355173f8
Valeria Kliui <valeriakliui@gmail.com>
2024-08-19 15:32:57 +0300

    fix: bind the router property db to the created app

--- server.cjs
+++ server.cjs
@@ -1,25 +1,18 @@
-// JSON Server module
 import jsonServer from "json-server";
 import authMiddleware from "json-server-auth";
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
+server.db = router.db;
 
+const middlewares = jsonServer.defaults();
 server.use(authMiddleware);
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  // Add custom route here if needed
-  jsonServer.rewriter({})
-);
+
+server.use(jsonServer.rewriter({}));
 server.use(router);
-// Listen to port
+
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
-
-// Export the Server API
-module.exports = server;
